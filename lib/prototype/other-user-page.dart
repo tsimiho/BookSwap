5afc28da0f5d0a0401ac79d78841f1d6b90b4d86
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-01-16 18:52:12 +0200

    changes other user page 2

--- lib/prototype/other-user-page.dart
+++ lib/prototype/other-user-page.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
 import '../assets/stacked-card.dart';
+import '../assets/stacked-card.dart';
 
 class Scene extends StatelessWidget {
   @override
@@ -68,65 +69,289 @@ class Scene extends StatelessWidget {
                         borderRadius: BorderRadius.circular(12 * fem),
                       ),
                       child: Container(
-                          // contentcontainerme1 (I54:11297;52350:27879)
+                        // contentcontainerme1 (I54:11297;52350:27879)
+                        padding: EdgeInsets.fromLTRB(
+                            16 * fem, 0 * fem, 0 * fem, 0 * fem),
+                        width: double.infinity,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xffffffff)),
+                          borderRadius: BorderRadius.circular(12 * fem),
+                        ),
+                        child: Container(
+                          // headerGam (I54:11297;52350:27881)
                           padding: EdgeInsets.fromLTRB(
-                              16 * fem, 0 * fem, 0 * fem, 0 * fem),
-                          width: double.infinity,
+                              0 * fem, 18 * fem, 0 * fem, 18 * fem),
+                          width: 344 * fem,
                           height: double.infinity,
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Color(0xffffffff)),
-                            borderRadius: BorderRadius.circular(12 * fem),
-                          ),
                           child: Container(
-                              // frame16To7 (65:12474)
-                              margin: EdgeInsets.fromLTRB(
-                                  14 * fem, 0 * fem, 14 * fem, 0 * fem),
-                              width: double.infinity,
-                              height: 485 * fem,
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Container(
-                                      // autogroupa5c5jkd (UPukFH62TGXWDT48rGA5C5)
-                                      width: double.infinity,
-                                      height: 308 * fem,
-                                      child: Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          StackedCard(),
-                                          Container(
-                                            width: 12 * fem,
-                                            height: 308 * fem,
-                                          ),
-                                          StackedCard()
-                                        ],
+                            // contentZpm (I54:11297;52350:27882)
+                            width: 141 * fem,
+                            height: double.infinity,
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Container(
+                                  // monogrameLR (I54:11297;52350:27883)
+                                  margin: EdgeInsets.fromLTRB(
+                                      0 * fem, 2 * fem, 26 * fem, 2 * fem),
+                                  width: 40 * fem,
+                                  height: double.infinity,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffe46962),
+                                    borderRadius:
+                                        BorderRadius.circular(20 * fem),
+                                  ),
+                                  child: Center(
+                                    child: Center(
+                                      child: Text(
+                                        'A',
+                                        textAlign: TextAlign.center,
+                                        style: SafeGoogleFont(
+                                          'Roboto',
+                                          fontSize: 16 * ffem,
+                                          fontWeight: FontWeight.w500,
+                                          height: 1.5 * ffem / fem,
+                                          letterSpacing: 0.150000006 * fem,
+                                          color: Color(0xfffffbfe),
+                                        ),
                                       ),
                                     ),
-                                    SizedBox(
-                                      height: 10 * fem,
-                                    ),
-                                    Row(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                      children: [
-                                        StackedCard(),
-                                        Container(
-                                          width: 12 * fem,
-                                          height: 308 * fem,
+                                  ),
+                                ),
+                                Container(
+                                  // textQU1 (I54:11297;52350:27886)
+                                  height: double.infinity,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        // headerjmB (I54:11297;52350:27887)
+                                        margin: EdgeInsets.fromLTRB(
+                                            0 * fem, 0 * fem, 0 * fem, 4 * fem),
+                                        child: Text(
+                                          'Username',
+                                          style: SafeGoogleFont(
+                                            'Roboto',
+                                            fontSize: 16 * ffem,
+                                            fontWeight: FontWeight.w500,
+                                            height: 1.5 * ffem / fem,
+                                            letterSpacing: 0.150000006 * fem,
+                                            color: Color(0xff1c1b1f),
+                                          ),
                                         ),
-                                        StackedCard()
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: 10 * fem,
-                                    ),
-                                  ]))),
+                                      ),
+                                      Text(
+                                        // subheadbYV (I54:11297;52350:27888)
+                                        'Distance',
+                                        style: SafeGoogleFont(
+                                          'Roboto',
+                                          fontSize: 12 * ffem,
+                                          fontWeight: FontWeight.w400,
+                                          height: 1.3333333333 * ffem / fem,
+                                          letterSpacing: 0.400000006 * fem,
+                                          color: Color(0xff1c1b1f),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
+            Container(
+              // autogroupaardtGh (UPscqEtnDKJoTZc3AfAard)
+              width: double.infinity,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    // snackbardarkY6M (I54:11364;51:5969)
+                    margin: EdgeInsets.fromLTRB(
+                        0 * fem, 21 * fem, 83 * fem, 0 * fem),
+                    width: 127 * fem,
+                    height: 55 * fem,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Color(0xffffffff)),
+                      color: Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(4 * fem),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Book List',
+                        style: SafeGoogleFont(
+                          'Cookie',
+                          fontSize: 24 * ffem,
+                          fontWeight: FontWeight.w400,
+                          height: 0.8333333333 * ffem / fem,
+                          letterSpacing: 0.25 * fem,
+                          color: Color(0xff0b0b0b),
+                        ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    // createaccountMJh (54:11354)
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.zero,
+                    ),
+                    child: Container(
+                      width: 118 * fem,
+                      height: 27 * fem,
+                      child: Container(
+                        // textfield4iu (I54:11354;38:1889)
+                        width: double.infinity,
+                        height: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20 * fem),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              // textfieldnQ1 (I54:11354;38:1889;52798:24431)
+                              margin: EdgeInsets.fromLTRB(
+                                  0 * fem, 0 * fem, 0 * fem, 33 * fem),
+                              width: double.infinity,
+                              height: 27 * fem,
+                              decoration: BoxDecoration(
+                                color: Color(0xffe46962),
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(4 * fem),
+                                  topRight: Radius.circular(4 * fem),
+                                ),
+                              ),
+                              child: Container(
+                                // statelayerfih (I54:11354;38:1889;52798:24432)
+                                padding: EdgeInsets.fromLTRB(
+                                    16 * fem, 5.5 * fem, 17 * fem, 3 * fem),
+                                width: double.infinity,
+                                height: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(4 * fem),
+                                    topRight: Radius.circular(4 * fem),
+                                  ),
+                                ),
+                                child: Container(
+                                  // contentwgD (I54:11354;38:1889;52798:24433)
+                                  width: double.infinity,
+                                  height: double.infinity,
+                                  child: Stack(
+                                    children: [
+                                      Positioned(
+                                        // labeltextT8m (I54:11354;38:1889;52798:24435)
+                                        left: 0 * fem,
+                                        top: 2.5 * fem,
+                                        child: Align(
+                                          child: SizedBox(
+                                            width: 42 * fem,
+                                            height: 16 * fem,
+                                            child: Text(
+                                              'Label 1',
+                                              style: SafeGoogleFont(
+                                                'Roboto',
+                                                fontSize: 12 * ffem,
+                                                fontWeight: FontWeight.w400,
+                                                height:
+                                                    1.3333333333 * ffem / fem,
+                                                letterSpacing:
+                                                    0.400000006 * fem,
+                                                color: Color(0xff49454f),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        // inputtext5R3 (I54:11354;38:1889;52798:24437)
+                                        left: 1 * fem,
+                                        top: 0 * fem,
+                                        child: Align(
+                                          child: SizedBox(
+                                            width: 84 * fem,
+                                            height: 16 * fem,
+                                            child: Text(
+                                              'Send Message',
+                                              style: SafeGoogleFont(
+                                                'Roboto',
+                                                fontSize: 12 * ffem,
+                                                fontWeight: FontWeight.w400,
+                                                height:
+                                                    1.3333333333 * ffem / fem,
+                                                letterSpacing:
+                                                    0.400000006 * fem,
+                                                color: Color(0xffffffff),
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                // frame16To7 (65:12474)
+                margin:
+                    EdgeInsets.fromLTRB(14 * fem, 0 * fem, 14 * fem, 0 * fem),
+                width: double.infinity,
+                height: 485 * fem,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        // autogroupa5c5jkd (UPukFH62TGXWDT48rGA5C5)
+                        width: double.infinity,
+                        height: 308 * fem,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            StackedCard(),
+                            Container(
+                              width: 12 * fem,
+                              height: 308 * fem,
+                            ),
+                            StackedCard()
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10 * fem,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          StackedCard(),
+                          Container(
+                            width: 12 * fem,
+                            height: 308 * fem,
+                          ),
+                          StackedCard()
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10 * fem,
+                      ),
+                    ]))
           ],
         ),
       ),
