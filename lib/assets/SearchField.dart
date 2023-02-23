54ed6a8f8298430428001c7066d58543e5905690
Achilleas Tsimichodimos <el19140@mail.ntua.gr>
2023-02-23 22:02:21 +0200

    1st implementation of voice recognition

--- lib/assets/SearchField.dart
+++ lib/assets/SearchField.dart
@@ -3,6 +3,12 @@ import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/utils.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:speech_recognition/speech_recognition.dart';
+import 'package:text_to_speech/text_to_speech.dart';
+import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class SearchField extends StatefulWidget {
   @override
@@ -11,12 +17,44 @@ class SearchField extends StatefulWidget {
 
 class _SearchFieldState extends State<SearchField> {
   final textController = TextEditingController();
+  SpeechToText _speechToText = SpeechToText();
+  bool _speechEnabled = false;
+  String _lastWords = '';
 
   @override
   void initState() {
     super.initState();
-
+    _initSpeech();
     textController.addListener(() => setState(() {}));
+  }
+
+  /// This has to happen only once per app
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+    });
   }
 
   @override
@@ -37,7 +75,15 @@ class _SearchFieldState extends State<SearchField> {
           hintText: 'Search...',
           hintStyle: TextStyle(color: Color(0xff49454f)),
           floatingLabelBehavior: FloatingLabelBehavior.never,
-          labelText: 'Search...',
+          labelText: _speechToText.isListening
+              ? '$_lastWords'
+              // If listening isn't active but could be tell the user
+              // how to start it, otherwise indicate that speech
+              // recognition is not yet ready or not supported on
+              // the target device
+              : _speechEnabled
+                  ? 'Search...'
+                  : 'Speech not available',
           labelStyle: TextStyle(
               color: textController.text.isEmpty
                   ? Color(0xff49454f)
@@ -50,7 +96,9 @@ class _SearchFieldState extends State<SearchField> {
               ? IconButton(
                   icon: Icon(Icons.mic),
                   color: Color(0xffe46962),
-                  onPressed: () => {},
+                  onPressed: _speechToText.isNotListening
+                      ? _startListening
+                      : _stopListening,
                 )
               : IconButton(
                   icon: Icon(Icons.close),
